1379be3e9a28ce7a98bbe9946bc7c3215692e30c
nirupamkumar <mundra.nirupamkumar@gmail.com>
2023-04-25 18:41:29 +0530

    Update PaddleController script
    added ApplyPowerEffect & ReesetPowerEffect methods

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
-    public float moveSpeed = 10.0f;
+    public float defaultMoveSpeed = 10.0f;
     public float moveRange = 4.0f;
     public string inputAxis;
     public string playerTag;
+    private float currentMoveSpeed;
+    private float powerupDuration = 3.0f;
+    private bool hasPowerup;
+
+    private void Start()
+    {
+        currentMoveSpeed = defaultMoveSpeed;
+    }
 
     private void Update()
     {
         float input = Input.GetAxis(inputAxis);
 
         Vector3 pos = transform.position;
-        pos.y += input * moveSpeed * Time.deltaTime;
+        pos.y += input * currentMoveSpeed * Time.deltaTime;
         pos.y = Mathf.Clamp(pos.y, -moveRange, moveRange);
 
         transform.position = pos;
+    }
+
+    public void ApplyPowerEffect(float powerupMoveSpeed)
+    {
+        currentMoveSpeed = powerupMoveSpeed;
+        hasPowerup = true;
+        StartCoroutine(ResetPowerEffect());
+    }
+
+    private IEnumerator ResetPowerEffect()
+    {
+        yield return new WaitForSeconds(powerupDuration);
+        currentMoveSpeed = defaultMoveSpeed;
+        hasPowerup = false;
+    }
+
+    public bool HasPowerEffect()
+    {
+        return hasPowerup;
     }
 }
