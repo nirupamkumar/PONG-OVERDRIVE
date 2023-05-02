b5553020396664f8c19f28e8125878888b8992f0
nirupamkumar <mundra.nirupamkumar@gmail.com>
2023-05-02 11:33:43 +0530

    Pause game script

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+   
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (Time.timeScale == 0)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1;
+    }
+}
