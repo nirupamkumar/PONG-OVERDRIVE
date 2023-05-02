89ff6087f439a9b6d0844a47bfcf7665f8f236fa
nirupamkumar <mundra.nirupamkumar@gmail.com>
2023-05-02 11:37:55 +0530

    Updated BallController.cs

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,26 +6,34 @@ public class BallController : MonoBehaviour
 {
     //ball movement
     public float speed = 5.0f;
-    private float playAreaWidth = 8.0f;
-    //public float playAreaHeight = 5.0f;
+    private float playAreaWidth = 8.2f; //public float playAreaHeight = 5.0f;
     private Vector3 ballDirection;
     private float ballMovementSpeed;
 
-    //storing the paddle information last ball touches
-    public GameObject paddle1;
-    public GameObject paddle2;
-    public GameObject lastHitPaddle;
+    private bool sinWaveActive = false;
+    private float sinWaveduration = 2.5f;
 
     private void Start()
     {
-        //ballDirection = new Vector3 (1.0f, 0.5f, 0).normalized;
         ballDirection = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f)).normalized;
         ballMovementSpeed = speed;
     }
 
     private void Update()
     {
-        transform.position += ballDirection * ballMovementSpeed * Time.deltaTime;
+
+        if (sinWaveActive)
+        {
+            //Amplitude can be adjusted from 0.3f to 0.5f
+            float yPosition = Mathf.Sin(Time.time * Mathf.PI * 2 / sinWaveduration) * 0.3f;
+            transform.position += new Vector3(ballDirection.x, yPosition, ballDirection.z) * ballMovementSpeed * Time.deltaTime;
+
+            //Debug.Log("sin wave power activated");
+        }
+        else
+        {
+            transform.position += ballDirection * ballMovementSpeed * Time.deltaTime;
+        }
 
         /*
         if (transform.position.y > playAreaHeight || transform.position.y > -playAreaWidth)
@@ -54,40 +62,63 @@ public class BallController : MonoBehaviour
         {
             ResetBall(-1);
         }
-        else if(transform.position.x < -playAreaWidth)
+        else if (transform.position.x < -playAreaWidth)
         {
             ResetBall(1);
         }
     }
 
+    /*
     IEnumerator ResetBallDelay(float delay, int direction)
     {
-        yield return new WaitForSeconds(delay);
-
         transform.position = Vector3.zero;
+
+        yield return new WaitForSecondsRealtime(delay);
+
         ballDirection = new Vector3(direction, Random.Range(-0.5f, 0.5f), 0f).normalized;
         ballMovementSpeed = speed;
     }
 
     void ResetBall(int direction)
     {
-        StartCoroutine(ResetBallDelay(0.1f, direction));
+        StartCoroutine(ResetBallDelay(2.0f, direction));
+    }
+    */
+
+    void ResetBall(int direction)
+    {
+        /*
+        float newXPosition = direction * playAreaWidth;
+        float newYPosition = Random.Range(-0.5f, 0.5f);
+        transform.position = new Vector3(newXPosition, newYPosition, 0f);
+        ballDirection = new Vector3(-direction, newYPosition, 0f).normalized;
+        ballMovementSpeed = speed; */
+
+        transform.position = Vector3.zero;
+        ballDirection = new Vector3(direction, Random.Range(-0.5f, 0.5f), 0f).normalized;
+        ballMovementSpeed = speed;
     }
 
     private void OnCollisionEnter(Collision col)
     {
         ballDirection = Vector3.Reflect(ballDirection, col.contacts[0].normal);
-
-        if (col.gameObject == paddle1 || col.gameObject == paddle2)
-        {
-            lastHitPaddle = col.gameObject;
-        }
     }
 
+    public void SinWaveActivate(bool sineWaveOnOff)
+    {
+        sinWaveActive = sineWaveOnOff;
+        StartCoroutine(ResetSinWave());
+    }
 
-    //Power implmentations here
-    //Effect activate and deactivatepower implmentations also here
-    //when the ball touches powerup which will spawn in gamemanager effect will activate
-    //when the ball collide with anything the power will deactivate
+    IEnumerator ResetSinWave()
+    {
+        yield return new WaitForSecondsRealtime(1.5f);
+        sinWaveActive = false;
+    }
+
+    public bool HasSinWavePowerEffect()
+    {
+        return sinWaveActive;
+    }
 
 }
