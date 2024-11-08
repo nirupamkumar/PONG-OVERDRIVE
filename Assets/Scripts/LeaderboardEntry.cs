5f325d8f57b3c3e9458b0311db3ce3a560a97b0e
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-11-08 22:59:13 +0100

    Create LeaderboardEntry.cs

---
+++ Assets/Scripts/LeaderboardEntry.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class LeaderBoardEntry
+{
+    public string gameNumber;
+    public string boardType;
+    public string dateTime;
+    public string playerScores;
+    public string winner;
+}
