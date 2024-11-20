fdac4433225d105c001fdfd8a68d369946e3b948
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-11-20 23:35:10 +0100

    Create DataForLeaderboard.cs

---
+++ Assets/Scripts/DataForLeaderboard.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataForLeaderboard : MonoBehaviour
+{
+    public string boardType;
+    public string p1Identifier;
+    public string p2Identifier;
+    public string winner;
+
+    // string value to declare for winner
+}
